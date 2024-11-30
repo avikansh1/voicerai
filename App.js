6229986e02db692008cebf8fc4f0257ce316712c
Avikansh1 <avikanshtiwari@gmai;.com>
2024-11-30 15:33:08 +0530

    Setting Up Project

---
+++ App.js
@@ -0,0 +1,11 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import SignUp from './components/authentication/SignUp';
+
+const App = () => {
+  return <SignUp />;
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ components/authentication/SignUp.js
@@ -0,0 +1,19 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <View style={styles.mainContainer}>
+      <Text>SignUp</Text>
+    </View>
+  );
+};
+
+export default SignUp;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
